d06f4fbc37787f63a4f4b29932f570a177dbe4f4
Fluff18 <rahulkoushik1999@gmail.com>
2023-02-09 00:51:34 -0700

    Added references

--- fovus-challenge-app/src/components/Form.jsx
+++ fovus-challenge-app/src/components/Form.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import fovusLogo from '../assets/FovusLogo.jpg';
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-// const onSubmit = (data) => {
-// console.log(data)
-// }
 
 export default function Form() {
-
-    // const { register, handleSubmit } = useForm()
-
-    // const onSubmit = (data) => {
-    //     console.log(data)
-    // }
 
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
@@ -23,6 +14,7 @@ export default function Form() {
             <div className='bg-gray-300 flex flex-col justify-center'>
                 <form className='max-w-[500px] w-full mx-auto bg-gray-100 p-8 px-8 rounded-lg' >
                     <h2 className='text-4xl dark:text-black font-bold text-center'>Fovus Challenge Form</h2>
+
                     <div className='flex flex-col text-gray-700 py-2'>
                         <label>Text Input</label>
                         <input className='rouded-lg bg-white mt-2 p-2 focus-within:border-black' type="text"/>
@@ -32,6 +24,7 @@ export default function Form() {
                         <label>File Input</label>
                         <input type='file' name='input_file'/>
                     </div>
+
                     <div className='flex justify-between text-gray-900 py-2'>
                         <p>Please attach a File</p>
                     </div>
